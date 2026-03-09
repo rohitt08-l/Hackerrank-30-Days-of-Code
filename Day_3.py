36126fea5014c09024841009f4ca7fca242dc07d
Rohit Patil <itzrohitpatil08@gmail.com>
2026-03-09 19:41:41 +0530

    Solved Day 3 Problem

---
+++ Day_3.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+def check(N):
+
+    if N%2==1:
+        print("Weird")
+    if N%2==0 and 2<=N<=5:
+        print("Not Weird")
+    if N%2==0 and 6<=N<=20:
+        print("Weird")
+    if N%2==0 and N>20:
+        print("Not Weird")
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    check(N)
